fe0a104ef5f18d4acd596bb35406237936b3758b
hsuting <hsuting0106@gmail.com>
2017-02-11 15:04:40 -0800

    add bot-fb

--- README.md
+++ README.md
@@ -5,10 +5,47 @@ Middleware of koa server.
 - koa-react
   - Need to install `nunjucks`, `react`, `react-dom` and `cat-components`.
   - Render `html` with `react` and `nunjucks`.
+  - Example:
+
+  ```javascript
+  import react from 'cat-middleware/lib/koa-react';
+  import Index from 'components/Index';
+
+  ...
+  app.use(react({
+    component: Index,
+    js: 'index',
+    ENV: true // use to minify html
+  }));
+  ...
+  ```
+
 - koa-relay
   - Need to install `nunjucks`, `react`, `react-dom`, `isomorphic-relay` and `cat-components`.
   - Render `html` with `react`, `relay` and `nunjucks`.
-- See more information in [wiki](https://github.com/HsuTing/cat-middleware/wiki).
+  - Example:
+
+  ```javascript
+  import Relay from 'react-relay';
+  import relay from 'cat-middleware/lib/koa-relay';
+  import index from 'containers/index';
+
+  const graphqlLink = 'http://localhost:8000/graphql';
+
+  ...
+  app.use(relay({
+    rootContainerProps: index({input: 'index'}),
+    networkLayer: new Relay.DefaultNetworkLayer(graphqlLink),
+    js: 'index',
+    ENV: true // use to minify html
+  }));
+  ...
+  ```
+
+- koa-bot-fb
+  - This use `koa-better-router`, you need to use `loadMethods()`.
+  - Need to install `koa-body`, `koa-better-router` and `request`.
+- See more information in [code](./src).
 
 ## License
 MIT © [hsuting](http://hsuting.com)
---
+++ src/koa-bot-fb.js
@@ -0,0 +1,84 @@
+'use strict';
+
+import process from 'process';
+import body from 'koa-body';
+import Router from 'koa-better-router';
+import request from 'request';
+
+export default receivedMessage => {
+  const router = Router().loadMethods();
+
+  router.get('/webhook', body(), ctx => {
+    const {request, response} = ctx;
+    if(request.query['hub.mode'] === 'subscribe' &&
+      request.query['hub.verify_token'] === process.env.BOT_FB_VERIFY_TOKEN) {
+      console.log('Validating webhook');
+
+      response.status = 200;
+      response.body = request.query['hub.challenge'];
+    } else {
+      console.error('Failed validation. Make sure the validation tokens match.');
+      response.status = 403;
+    }
+  });
+
+  router.post('/webhook', body(), ctx => {
+    const {request, response} = ctx;
+    const data = request.body;
+
+    if(data.object === 'page') {
+      data.entry.forEach(entry => {
+        entry.messaging.forEach(event => {
+          if(event.message)
+            receivedMessage(event);
+          else
+            console.log(`Webhook received unknown event: ${event}`);
+        });
+      });
+
+      response.status = 200;
+    }
+  });
+};
+
+export const callSendAPI = messageData => {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: process.env.BOT_FB_PAGE_ACCESS_TOKEN
+    },
+    method: 'POST',
+    json: messageData
+  }, (error, response, body) => {
+    if(!error && response.statusCode == 200) {
+      const recipientId = body.recipient_id;
+      const messageId = body.message_id;
+
+      console.log(`Successfully sent generic message with id ${messageId} to recipient ${recipientId}`);
+    } else {
+      console.error('Unable to send message.');
+      console.error(response);
+      console.error(error);
+    }
+  });
+};
+
+export const sendTextMessage = (recipientId, messageText) => {
+  callSendAPI({
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  });
+};
+
+export const sendMessage = (recipientId, message) => {
+  callSendAPI({
+    recipient: {
+      id: recipientId
+    },
+    message
+  });
+};
